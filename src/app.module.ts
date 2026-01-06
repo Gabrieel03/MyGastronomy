09c5aa1c0e6c30ac2e6ce009dd8857c354355604
Gabriel <gabriel_.brito@hotmail.com>
2026-01-05 22:08:16 -0300

    Conecção com banco de dados e estruturação de users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as "mongodb",
+      url: process.env.DATABASE_URL,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
