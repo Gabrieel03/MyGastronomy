02a73c13a4498ad48bca11fde8eebc1a76c9a5e1
Gabriel <gabriel_.brito@hotmail.com>
2026-01-08 03:18:19 -0300

    feat: Falta de validação em dto por isso estava dando erro

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @ApiProperty({ example: 'gabriel@email.com' })
+  @IsEmail({}, {message: 'Email inválido'})
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty({ example: '123456' })
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
